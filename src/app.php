abe8b6321d2d0a2e2708fc5503f5d55005ef2718
safwan39 <53286721+safwan39@users.noreply.github.com>
2021-04-26 15:52:11 +0800

    update html page

--- src/app.php
+++ src/app.php
@@ -6,6 +6,8 @@ use React\Component;
 class App extends Component{
     function render(){
         return [
+            new meta(['name'=> 'viewport', 'content'=> "width=device-width, initial-scale=1"]),
+            new title('PHP-REACT SAMPLE'),
             new link(['href'=> 'https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css', 'rel'=> 'stylesheet']),
             new Header,
             new Content,
